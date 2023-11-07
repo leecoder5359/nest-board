99f3492ead1087826d6258f9cf0d1cb9390da225
이호준 <leecoder5359@gmail.com>
2023-11-08 03:35:59 +0900

    (feat): app e2e(appController) 추가

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,79 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { ExecutionContext, INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { AppModule } from '../src/app.module';
+import { AuthService } from '../src/modules/auth/auth.service';
+import { LocalAuthGuard } from '../src/modules/auth/guards/local-auth.guard';
+import { JwtAuthGuard } from '../src/modules/auth/guards/jwt-auth.guard';
 
 describe('AppController (e2e)', () => {
-  let app: INestApplication;
+    let app: INestApplication;
+    let authService: AuthService;
+    let localAuthGuard: LocalAuthGuard;
+    let jwtAuthGuard: JwtAuthGuard;
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
+    beforeEach(async () => {
+        const moduleFixture: TestingModule = await Test.createTestingModule({
+            imports: [AppModule],
+        })
+            .overrideGuard(JwtAuthGuard)
+            .useValue({
+                canActivate: (context: ExecutionContext) => {
+                    context.switchToHttp().getRequest().getContext().req.user = {
+                        id: 1,
+                        email: 'test@test.com',
+                        password: '12345678',
+                    };
+                    return true;
+                },
+            })
+            .compile();
 
-    app = moduleFixture.createNestApplication();
-    await app.init();
-  });
+        app = moduleFixture.createNestApplication();
+        await app.init();
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
-  });
+        authService = moduleFixture.get<AuthService>(AuthService);
+        localAuthGuard = moduleFixture.get<LocalAuthGuard>(LocalAuthGuard);
+        jwtAuthGuard = moduleFixture.get<JwtAuthGuard>(JwtAuthGuard);
+    });
+
+    it('/ (GET)', async () => {
+        const response = await request(app.getHttpServer()).get('/');
+
+        expect(response.status).toBe(200);
+        expect(response.body).toBeDefined();
+    });
+
+    it('/login (POST) - with valid credentials', async () => {
+        jest.spyOn(localAuthGuard, 'canActivate').mockImplementation(() => true);
+        jest.spyOn(authService, 'login').mockResolvedValue({ accessToken: 'accessToken' });
+
+        const response = await request(app.getHttpServer()).post('/login').send({
+            email: 'test@test.com',
+            password: '12345678',
+        });
+
+        expect(response.status).toBe(201);
+        expect(response.body).toHaveProperty('accessToken');
+    });
+
+    it('/me (GET) - with JWT', async () => {
+        jest.spyOn(jwtAuthGuard, 'canActivate').mockImplementation(() => true);
+        const response = await request(app.getHttpServer())
+            .get('/me')
+            .set('Authorization', `Bearer accessToken`);
+
+        expect(response.status).toBe(200);
+        expect(response.body).toBeDefined();
+    });
+
+    it('/me (GET) - 인증 실패한 유저', async () => {
+        jest.spyOn(jwtAuthGuard, 'canActivate').mockImplementation(() => false);
+
+        const response = await request(app.getHttpServer())
+            .get('/me')
+            .set('Authorization', `Bearer accessToken`);
+
+        expect(response.status).toBe(403);
+    });
 });
